4a80156e6de6d8110fa06533bf2371d6807397a0
kiciax98 <kiciax98@gmail.com>
2021-03-25 00:56:06 +0100

    Repository holding map with list of song hits

---
+++ src/main/java/SongApp/repository/SongRepository.java
@@ -0,0 +1,27 @@
+package SongApp.repository;
+
+import SongApp.model.Song;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class SongRepository {
+
+    private Map<String, List<Song>> songMap;
+
+    public SongRepository() {
+        this.songMap = new HashMap<>();
+    }
+
+    public void addSongList(String radioName, List<Song> songList){
+        this.songMap.putIfAbsent(radioName, songList);
+    }
+
+    public List<Song> getSongList(String radioName){
+        return songMap.get(radioName);
+    }
+
+}
