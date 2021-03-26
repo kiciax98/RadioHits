96c7da411b0c137b407d126616d4e8193a72c2a3
kiciax98 <kiciax98@gmail.com>
2021-03-26 11:53:24 +0100

    Added Utils

--- src/main/java/SongApp/service/SongService.java
+++ src/main/java/SongApp/service/SongService.java
@@ -3,6 +3,7 @@ package SongApp.service;
 import SongApp.model.Song;
 import SongApp.model.SongArtist;
 import SongApp.repository.SongRepository;
+import SongApp.service.utils.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,195 +23,68 @@ public class SongService {
     }
 
     public List<Song> rmfmaxxHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "https://www.rmfmaxxx.pl/top40";
-            Document doc = Jsoup.connect(address).get();
-            Elements hits = doc.select("div.list-songs a.is-title");
-            int counter = 0;
-            for (Element element : hits) {
-                if(counter == 39){
-                    break;
-                }
-                String[] splitArtistsAndName = element.attr("title").split(" - ");
-                songList.add(new Song(splitArtistsAndName[1]));
-                String[] splitArtistsUsingSlash = splitArtistsAndName[0].split(" / ");
-                for (String string : splitArtistsUsingSlash) {
-                    songList.get(counter).addArtist(new SongArtist(string));
-                }
-                counter++;
-
-            }
-            songListMap.addSongList("rmfmaxx", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        RmfmaxxUtils rmfmaxxUtils = new RmfmaxxUtils();
+        String address = "https://www.rmfmaxxx.pl/top40";
+        Document document = rmfmaxxUtils.connectToAddress(address);
+        String cssQuery = "div.list-songs a.is-title";
+        Elements elements = rmfmaxxUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = rmfmaxxUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("rmfmaxx", songList);
         return songListMap.getSongList("rmfmaxx");
     }
 
     public List<Song> radiozetHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "https://www.radiozet.pl/Radio/Lista-przebojow";
-            Document doc = Jsoup.connect(address).get();
-            Elements hitArtist = doc.select("div.chart__full__list__track-list div.track div.track div.artist-track");
-
-            int counter = 0;
-            String[] artists = new String[30];
-            for (Element element : hitArtist) {
-                if (counter == 30) {
-                    break;
-                }
-                artists[counter] = element.text();
-                counter++;
-            }
-            counter = 0;
-            Elements hitNameAndFeat = doc.select("div.chart__full__list__track-list div.track div.track div.title-track");
-            for (Element element : hitNameAndFeat) {
-                if (counter == 30) {
-                    break;
-                }
-                String[] splitNameAndFeat = element.text().split(" \\(feat. ");
-                songList.add(new Song(splitNameAndFeat[0]));
-                songList.get(counter).addArtist(new SongArtist(artists[counter]));
-                if(splitNameAndFeat.length >1) {
-                    String[] splitIfMoreThanOneFeat = splitNameAndFeat[1].split(",");
-                    for (String string : splitIfMoreThanOneFeat) {
-                        songList.get(counter).addArtist(new SongArtist(string.replace(")","")));
-                    }
-                }
-                counter++;
-            }
-            songListMap.addSongList("radiozet", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        String address = "https://www.radiozet.pl/Radio/Lista-przebojow";
+        RadiozetUtils radiozetUtils = new RadiozetUtils(address);
+        Document document = radiozetUtils.connectToAddress(address);
+        String cssQuery = "div.chart__full__list__track-list div.track div.track div.artist-track";
+        Elements elements = radiozetUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = radiozetUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("radiozet", songList);
         return songListMap.getSongList("radiozet");
     }
 
     public List<Song> rmffmHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "https://www.rmf.fm/au/?a=poplista";
-            Document doc = Jsoup.connect(address).get();
-            Elements hits = doc.select("div.box-text div.poplista-artist-title");
-            int counter = 0;
-            for (Element element : hits) {
-                if (counter == 20) {
-                    break;
-                }
-                String text = element.text();
-                String[] splitArtistsAndName = text.split(": ");
-                songList.add(new Song(splitArtistsAndName[1]));
-                String[] splitArtistsUsingFeat = splitArtistsAndName[0].split(" feat. ");
-                for (String string : splitArtistsUsingFeat) {
-                    songList.get(counter).addArtist(new SongArtist(string));
-                }
-                counter++;
-            }
-            songListMap.addSongList("rmffm", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        RmffmUtils rmffmUtils = new RmffmUtils();
+        String address = "https://www.rmf.fm/au/?a=poplista";
+        Document document = rmffmUtils.connectToAddress(address);
+        String cssQuery = "div.box-text div.poplista-artist-title";
+        Elements elements = rmffmUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = rmffmUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("rmffm", songList);
         return songListMap.getSongList("rmffm");
     }
 
     public List<Song> eskaHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "https://www.eska.pl/goraca20/";
-            Document doc = Jsoup.connect(address).get();
-            Elements hitNames = doc.select("div.artist-hits a.single-hit__title");
-            int counter = 0;
-            for (Element element : hitNames) {
-                if (counter == 20) {
-                    break;
-                }
-                songList.add(new Song(element.text()));
-                counter++;
-            }
-            counter = 0;
-            Elements hitArtists = doc.select("div.artist-hits div.single-hit__info ul");
-            for (Element element : hitArtists) {
-                if (counter == 20) {
-                    break;
-                }
-                Elements artists = element.select("a");
-                for (Element element1 : artists) {
-                    songList.get(counter).addArtist(new SongArtist(element1.text()));
-                }
-                counter++;
-            }
-            songListMap.addSongList("eska", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        String address = "https://www.eska.pl/goraca20/";
+        EskaUtils eskaUtils = new EskaUtils(address);
+        Document document = eskaUtils.connectToAddress(address);
+        String cssQuery = "div.artist-hits a.single-hit__title";
+        Elements elements = eskaUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = eskaUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("eska", songList);
         return songListMap.getSongList("eska");
     }
 
     public List<Song> radiowawaHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "http://www.radiowawa.pl/top13";
-            Document doc = Jsoup.connect(address).get();
-            Elements hitNames = doc.select("div.notowanie-row span.title");
-            int counter = 0;
-            for (Element element : hitNames) {
-                songList.add(new Song(element.text()));
-                if (counter == 13) {
-                    break;
-                }
-                counter++;
-            }
-            Elements hitArtists = doc.select("div.notowanie-row span.artist");
-            counter = 0;
-            for (Element element : hitArtists) {
-                if (counter == 13) {
-                    break;
-                }
-                String[] split = element.text().split(",");
-                for (String s : split) {
-                    songList.get(counter).addArtist(new SongArtist(s));
-                }
-                counter++;
-            }
-            songListMap.addSongList("radiowawa", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        String address = "http://www.radiowawa.pl/top13";
+        RadiowawaUtils radiowawaUtils = new RadiowawaUtils(address);
+        Document document = radiowawaUtils.connectToAddress(address);
+        String cssQuery = "div.notowanie-row span.title";
+        Elements elements = radiowawaUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = radiowawaUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("radiowawa", songList);
         return songListMap.getSongList("radiowawa");
     }
 
     public List<Song> voxfmHitList() {
-        try {
-            List<Song> songList = new ArrayList<>();
-            String address = "https://www.voxfm.pl/bestlista/";
-            Document doc = Jsoup.connect(address).get();
-            Elements hitNames = doc.select("div.artist-hits a.single-hit__title");
-            int counter = 0;
-            for (Element element : hitNames) {
-                if (counter == 20) {
-                    break;
-                }
-                songList.add(new Song(element.text()));
-                counter++;
-            }
-            counter = 0;
-            Elements hitArtists = doc.select("div.artist-hits div.single-hit__info ul");
-            for (Element element : hitArtists) {
-                if (counter == 20) {
-                    break;
-                }
-                Elements artists = element.select("a");
-                for (Element element1 : artists) {
-                    songList.get(counter).addArtist(new SongArtist(element1.text()));
-                }
-                counter++;
-            }
-            songListMap.addSongList("voxfm", songList);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        String address = "https://www.voxfm.pl/bestlista/";
+        VoxfmUtils voxfmUtils = new VoxfmUtils(address);
+        Document document = voxfmUtils.connectToAddress(address);
+        String cssQuery = "div.artist-hits a.single-hit__title";
+        Elements elements = voxfmUtils.getSelectedElements(document, cssQuery);
+        List<Song> songList = voxfmUtils.getSongNameAndArtistsFromElements(elements);
+        songListMap.addSongList("voxfm", songList);
         return songListMap.getSongList("voxfm");
     }
 }
---
+++ src/main/java/SongApp/service/utils/VoxfmUtils.java
@@ -0,0 +1,44 @@
+package SongApp.service.utils;
+
+import SongApp.model.Song;
+import SongApp.model.SongArtist;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoxfmUtils implements Radio{
+
+    private final String address;
+
+    public VoxfmUtils(final String address) {
+        this.address = address;
+    }
+
+    @Override
+    public List<Song> getSongNameAndArtistsFromElements(Elements hits) {
+        List<Song> songList = new ArrayList<>();
+        int counter = 0;
+        for (Element element : hits) {
+            if (counter == 20) {
+                break;
+            }
+            songList.add(new Song(element.text()));
+            counter++;
+        }
+        counter = 0;
+        Elements hitArtists = getSelectedElements(connectToAddress(address), "div.artist-hits div.single-hit__info ul");
+        for (Element element : hitArtists) {
+            if (counter == 20) {
+                break;
+            }
+            Elements artists = element.select("a");
+            for (Element element1 : artists) {
+                songList.get(counter).addArtist(new SongArtist(element1.text()));
+            }
+            counter++;
+        }
+        return songList;
+    }
+}
