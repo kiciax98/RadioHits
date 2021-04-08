8c6986193b0236e2723d293b24bc76a83a370571
Krzysiek <kiciax98@gmail.com>
2021-04-08 15:46:56 +0200

    injecting dependency

--- src/main/java/SongApp/service/SongService.java
+++ src/main/java/SongApp/service/SongService.java
@@ -3,8 +3,7 @@ package SongApp.service;
 import SongApp.model.Song;
 import SongApp.repository.SongRepository;
 import SongApp.service.utils.*;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,75 +11,49 @@ import java.util.List;
 @Service
 public class SongService {
 
+    @Autowired
     private SongRepository songRepository;
 
-    public SongService() {
-        this.songRepository = new SongRepository();
-    }
+    @Autowired
+    private RadiowawaUtils radiowawaUtils;
+    @Autowired
+    private RadiozetUtils radiozetUtils;
+    @Autowired
+    private RmfmaxxUtils rmfmaxxUtils;
+    @Autowired
+    private RmffmUtils rmffmUtils;
+    @Autowired
+    private VoxfmUtils voxfmUtils;
+    @Autowired
+    private EskaUtils eskaUtils;
 
     public List<Song> rmfmaxxHitList() {
-        RmfmaxxUtils rmfmaxxUtils = new RmfmaxxUtils();
-        String address = "https://www.rmfmaxxx.pl/top40";
-        Document document = rmfmaxxUtils.connectToAddress(address);
-        String cssQuery = "div.list-songs a.is-title";
-        Elements elements = rmfmaxxUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = rmfmaxxUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("rmfmaxx", songList);
+        songRepository.addSongList("rmfmaxx", rmfmaxxUtils.getSongList());
         return songRepository.getSongList("rmfmaxx");
     }
 
     public List<Song> radiozetHitList() {
-        String address = "https://www.radiozet.pl/Radio/Lista-przebojow";
-        RadiozetUtils radiozetUtils = new RadiozetUtils(address);
-        Document document = radiozetUtils.connectToAddress(address);
-        String cssQuery = "div.chart__full__list__track-list div.track div.track div.artist-track";
-        Elements elements = radiozetUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = radiozetUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("radiozet", songList);
+        songRepository.addSongList("radiozet", radiozetUtils.getSongList());
         return songRepository.getSongList("radiozet");
     }
 
     public List<Song> rmffmHitList() {
-        RmffmUtils rmffmUtils = new RmffmUtils();
-        String address = "https://www.rmf.fm/au/?a=poplista";
-        Document document = rmffmUtils.connectToAddress(address);
-        String cssQuery = "div.box-text div.poplista-artist-title";
-        Elements elements = rmffmUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = rmffmUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("rmffm", songList);
+        songRepository.addSongList("rmffm", rmffmUtils.getSongList());
         return songRepository.getSongList("rmffm");
     }
 
-    public List<Song> eskaHitList() {
-        String address = "https://www.eska.pl/goraca20/";
-        EskaAndVoxfmUtils eskaAndVoxfmUtils = new EskaAndVoxfmUtils(address);
-        Document document = eskaAndVoxfmUtils.connectToAddress(address);
-        String cssQuery = "div.artist-hits a.single-hit__title";
-        Elements elements = eskaAndVoxfmUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = eskaAndVoxfmUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("eska", songList);
-        return songRepository.getSongList("eska");
-    }
-
     public List<Song> radiowawaHitList() {
-        String address = "http://www.radiowawa.pl/top13";
-        RadiowawaUtils radiowawaUtils = new RadiowawaUtils(address);
-        Document document = radiowawaUtils.connectToAddress(address);
-        String cssQuery = "div.notowanie-row span.title";
-        Elements elements = radiowawaUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = radiowawaUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("radiowawa", songList);
+        songRepository.addSongList("radiowawa", radiowawaUtils.getSongList());
         return songRepository.getSongList("radiowawa");
     }
 
+    public List<Song> eskaHitList() {
+        songRepository.addSongList("eska", eskaUtils.getSongList());
+        return songRepository.getSongList("eska");
+    }
+
     public List<Song> voxfmHitList() {
-        String address = "https://www.voxfm.pl/bestlista/";
-        EskaAndVoxfmUtils voxfmUtils = new EskaAndVoxfmUtils(address);
-        Document document = voxfmUtils.connectToAddress(address);
-        String cssQuery = "div.artist-hits a.single-hit__title";
-        Elements elements = voxfmUtils.getSelectedElements(document, cssQuery);
-        List<Song> songList = voxfmUtils.getSongNameAndArtistsFromElements(elements);
-        songRepository.addSongList("voxfm", songList);
+        songRepository.addSongList("voxfm", voxfmUtils.getSongList());
         return songRepository.getSongList("voxfm");
     }
 }
